8e9384579b61879ba57c1501fe21d6f4555cea7a
Alexej1900 <alexej1900@mail.ru>
2021-09-11 16:06:57 +0300

    init pointing-poker-server

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+//Initialize new socket.io instance and pass the http server to it
+const io = require('socket.io')(http)
+app.use(cors())
+io.on('connection', (socket) => {
+  socket.on("login", ({name, room}, callback) => {
+  
+  })
+  socket.on("sendMessage", message => {
+  
+  })
+  socket.on("disconnect", () => {
+  
+  })
+})
+app.get('/', (req, res) => {
+  req.send('Server is up and running')
+})
+http.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+})
